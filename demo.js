55195bb53f37a705860b282298393387f3882c4b
Samik Malhotra <samikmalhotra66@gmail.com>
2022-01-20 09:34:00 +0530

    initialised graphql

---
+++ demo.js
@@ -0,0 +1,14 @@
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+    type User{
+        email: String!,
+        avatar: String,
+        friends: [User!]!,
+    }
+
+    type Query{
+        me: User!
+    }
+     
+`
